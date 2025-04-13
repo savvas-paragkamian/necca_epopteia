ddb2f79823449e18ed593d6232c56d477f692363
savvas-paragkamian <s.paragkamian@gmail.com>
2025-04-13 18:14:06 +0300

    remove natura2000 code and keep only about occurrences and species names

--- scripts/species_enrichment.R
+++ scripts/species_enrichment.R
@@ -19,16 +19,11 @@ library(units)
 library(vegan)
 library(rnaturalearth)
 
-# Load data
+############################# Load data ############################
 ## borders for maps
-world <- ne_countries(scale = "medium", returnclass = "sf")
-
-country_name <- "Greece"
-greece <- world[world$name == country_name, ]
-
-hellenic_borders_shp <- sf::st_read("../spatial_data/hellenic_borders/hellenic_borders.shp") |>
-    st_transform(4326)
-
+greece_regions <- sf::st_read("../spatial_data/gadm41_GRC_shp/gadm41_GRC_2.shp")
+hellenic_borders_shp <- sf::st_read("../spatial_data/hellenic_borders/hellenic_borders.shp")
+########################## species information ##############################
 # the species of the previous epopteia 2015
 
 invertebrates_92_43 <- readxl::read_excel("../data/invertebrates_92_43.xlsx", skip=2, col_names=F)
@@ -77,175 +72,6 @@ species_names <- unique(invertebrates_92_43$SPECIES_NAME)
 #                   "Osmoderma eremita Complex",
 #                   "Hirudo verbana")
 
-## Natura2000 v32 version
-
-natura_v32_species <- read_delim("../data/Natura2000DB_V32_species.tsv",delim="\t")
-natura_v32_site <- read_delim("../data/Natura2000DB_V32_site.tsv",delim="\t")
-natura_v32_region <- read_delim("../data/Natura2000DB_V32_region.tsv",delim="\t")
-natura_v32_other_species <- read_delim("../data/Natura2000DB_V32_other_species.tsv",delim="\t")
-
-groups_df <- data.frame(SPECIES_GROUP=c("R","B","A","M","I","F","P"),
-                    groups_names=c("Reptiles", "Birds", "Amphibians", "Mammals", "Invertebrates", "Fish", "Plants"))
-site_types_df <- data.frame(SITE_TYPE=c("A","B","C"), SITE_TYPE_NAME=c("SPAs","SCIs_SACs", "SPAs_and_SCIs_SACs"))
-
-################################ Descriptives ###################################
-##  
-all_species <- unique(c(unique(natura_v32_species$SPECIES_NAME),unique(natura_v32_other_species$OTHER_SPECIES_NAME)))
-
-natura_v32_species_sum <- natura_v32_species |> 
-    distinct(SPECIES_NAME,SPECIES_GROUP,SITE_CODE) 
-
-natura_v32_other_species_sum <- natura_v32_other_species |> 
-    distinct(OTHER_SPECIES_NAME,OTHER_SPECIES_GROUP, SITE_CODE)
-
-colnames(natura_v32_other_species_sum) <- colnames(natura_v32_species_sum)
-
-natura_v32_all_species <- rbind(natura_v32_other_species_sum, natura_v32_species_sum) |>
-    distinct()
-
-### species
-groups_species_summary <- natura_v32_all_species |>
-    distinct(SPECIES_NAME, SPECIES_GROUP) |>
-    group_by(SPECIES_GROUP) |> 
-    summarise(n_species=n()) |>
-    left_join(groups_df)
-
-invertebrates_all <- natura_v32_all_species |>
-    filter(SPECIES_GROUP=="I")
-
-invertebrates_all_natura_summary <- invertebrates_all |>
-    group_by(SITE_CODE) |>
-    summarise(n_species=n()) |> 
-    left_join(natura_v32_region)
-
-invertebrates_all_natura_summary_region <- invertebrates_all_natura_summary |>
-    group_by(REGION_NAME) |>
-    summarise(n_species=sum(n_species), n_sites=n())
-
-invertebrates_all_species <- invertebrates_all |>
-    distinct(SPECIES_NAME)
-
-### regions
-## some sites are in multiple regions 
-# > natura_v32_region |> distinct(REGION_CODE,SITE_CODE) |> group_by(SITE_CODE) |> summarise(n=n()) |> arrange(desc(n))
-
-## overlap
-## 16 not included, 23 included
-invertebrates_not_in_natura <- species_names[which(!(species_names %in% natura_v32_species$SPECIES_NAME))]
-invertebrates_not_in_other_natura <- species_names[which(!(species_names %in% natura_v32_other_species$OTHER_SPECIES_NAME))]
-
-
-invertebrates_not_everywhere <- species_names[which(!(species_names %in% all_species))] 
-
-species_names %in% unique(natura_v32_other_species$OTHER_SPECIES_NAME)
-
-## Species from excel statistics
-
-natura_v32_all_species_excel <- natura_v32_all_species |>
-    filter(SPECIES_NAME %in% species_names) |>
-    group_by(SPECIES_NAME) |>
-    summarise(n_sites=n(),SITE_CODE=str_c(SITE_CODE, collapse = ","))
-
-write_delim(natura_v32_all_species_excel, "../results/natura_v32_all_species_excel.tsv", delim="\t")
-
-### sites
-
-groups_sites_summary <- natura_v32_all_species |>
-    distinct(SITE_CODE, SPECIES_GROUP,SPECIES_NAME) |>
-    group_by(SITE_CODE,SPECIES_GROUP) |> 
-    summarise(n_species=n(),
-              species_names=str_c(SPECIES_NAME,collapse = ","),
-              .groups="keep") |>
-    left_join(natura_v32_site) |>
-    left_join(site_types_df)
-
-write_delim(groups_sites_summary,
-            "../results/natura_groups_summary_all.tsv",
-            delim="\t")
-
-groups_sites_summary_invertebrates <- groups_sites_summary |>
-    filter(SPECIES_GROUP=="I")
-
-write_delim(groups_sites_summary_invertebrates,
-            "../results/natura_groups_summary_invertebrates.tsv",
-            delim="\t")
-
-#### sites
-groups_site_type_summary <- natura_v32_all_species |>
-    left_join(natura_v32_site) |>
-    left_join(site_types_df) |>
-    distinct(SITE_TYPE_NAME, SPECIES_GROUP,SITE_CODE) |>
-    left_join(groups_df) |>
-    group_by(SITE_TYPE_NAME,groups_names) |>
-    summarise(n_sites=n(), .groups="keep") 
-
-groups_site_type_sites_summary_all <- natura_v32_all_species |>
-    left_join(natura_v32_site) |>
-    left_join(site_types_df) |>
-    distinct(SITE_TYPE_NAME, SITE_CODE) |>
-    group_by(SITE_TYPE_NAME) |>
-    summarise(n_sites=n()) |>
-    mutate(groups_names="All") |>
-    rbind(groups_site_type_summary)
-
-#### type species
-groups_site_type_species_summary <- natura_v32_all_species |>
-    left_join(natura_v32_site) |>
-    left_join(site_types_df) |>
-    distinct(SITE_TYPE_NAME, SPECIES_GROUP,SPECIES_NAME) |>
-    left_join(groups_df) |>
-    group_by(SITE_TYPE_NAME,groups_names) |>
-    summarise(n_species=n(), .groups="keep") 
-
-groups_site_type_species_summary_all <- natura_v32_all_species |>
-    left_join(natura_v32_site) |>
-    left_join(site_types_df) |>
-    distinct(SITE_TYPE_NAME,SPECIES_NAME) |>
-    group_by(SITE_TYPE_NAME) |>
-    summarise(n_species=n()) |>
-    mutate(groups_names="All") |>
-    rbind(groups_site_type_species_summary)
-
-
-### species from annex II
-groups_sites_invertebrates <- groups_sites_summary |>
-    filter(SPECIES_GROUP=="I")
-
-species_annex_II <- invertebrates_92_43 |>
-    filter(!is.na(ANNEX_II))
-
-groups_site_type_species_annex_II <- natura_v32_all_species |>
-    filter(SPECIES_NAME %in% species_annex_II$SPECIES_NAME) |>
-    left_join(natura_v32_site) |>
-    left_join(site_types_df) |>
-    distinct(SITE_TYPE_NAME, SPECIES_GROUP,SPECIES_NAME) |>
-    mutate(groups_names="Invertebrates_annex_II") |>
-    group_by(SITE_TYPE_NAME,groups_names) |>
-    summarise(n_species=n(), .groups="keep") 
-
-groups_site_type_annex_II_summary <- natura_v32_all_species |>
-    filter(SPECIES_NAME %in% species_annex_II$SPECIES_NAME) |>
-    left_join(natura_v32_site) |>
-    left_join(site_types_df) |>
-    distinct(SITE_TYPE_NAME, SPECIES_GROUP,SITE_CODE) |>
-    mutate(groups_names="Invertebrates_annex_II") |>
-    group_by(SITE_TYPE_NAME,groups_names) |>
-    summarise(n_sites=n(), .groups="keep") 
-
-natura_sites_groups_species_annex_II <- groups_site_type_species_annex_II |>
-    left_join(groups_site_type_annex_II_summary,
-    by=c("SITE_TYPE_NAME","groups_names"))
-
-### sites and species together
-
-natura_sites_groups_species_summary <- groups_site_type_sites_summary_all |>
-    left_join(groups_site_type_species_summary_all,
-              by=c("SITE_TYPE_NAME","groups_names")) |>
-    rbind(natura_sites_groups_species_annex_II)
-
-write_delim(natura_sites_groups_species_summary,
-            "../results/natura_sites_groups_species_summary.tsv",
-            delim="\t")
 
 ################################ Enrichment ###################################
 # GBIF retrieve data for all arthropod species that have been assessed in IUCN
@@ -277,18 +103,19 @@ gnr_species_gbif <- gnr_resolve(species_gbif_df$sci_name, data_source_ids=11)
 
 write_delim(gnr_species, "../results/gnr_species_names.tsv", delim="\t")
 
+########################## species occurrences ##############################
 # GBIF occurrences
 ## need to set GBIF credential in the .Renviron file
 
-gbif_taxon_keys <- as.numeric(na.omit(species_gbif_df$gbifid.ids))
-
+#gbif_taxon_keys <- as.numeric(na.omit(species_gbif_df$gbifid.ids))
+#
 ## run once to request the download from the server
-occ_download(
-pred_in("taxonKey", gbif_taxon_keys), # important to use pred_in
-pred("hasCoordinate", TRUE),
-pred("hasGeospatialIssue", FALSE),
-format = "SIMPLE_CSV"
-)
+#occ_download(
+#pred_in("taxonKey", gbif_taxon_keys), # important to use pred_in
+#pred("hasCoordinate", TRUE),
+#pred("hasGeospatialIssue", FALSE),
+#format = "SIMPLE_CSV"
+#)
 
 # to check the status of the download
 # This is for the species of annex II 
@@ -296,11 +123,12 @@ format = "SIMPLE_CSV"
 # the key for the gbif download of 268 invertegrate species 
 # is 0018673-241107131044228 
 
-gbif_species_occ <- occ_download_get('0018673-241107131044228') |>
-    occ_download_import()
+#gbif_species_occ <- occ_download_get('0018673-241107131044228') |>
+#    occ_download_import()
 
-write_delim(gbif_species_occ, "../results/gbif_invertebrate_species_occ.tsv", delim="\t")
+#write_delim(gbif_species_occ, "../results/gbif_invertebrate_species_occ.tsv", delim="\t")
 
+gbif_species_occ <- read_delim("../results/gbif_invertebrate_species_occ.tsv", delim="\t")
 
 gbif_species_occ_sf <- gbif_species_occ |> 
     st_as_sf(coords=c("decimalLatitude","decimalLongitude"),
@@ -374,7 +202,6 @@ ggsave("../figures/gbif_species_gr_map.png",
        device="png")
 
 
-
 ######################
 # Define bounding box as sf object for visual checks if needed
 bbox <- st_bbox(c(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax), crs = 4326)
@@ -397,3 +224,241 @@ points_outside <- st_as_sf(data.frame(
 
 # Combine the points into one sf object for easy handling
 all_points <- rbind(points_inside, points_outside)
+
+
+
+############################################################################################
+#################### Species Occurrences Data Homogenisation ###############################
+############################################################################################
+#### Species list
+invertebrates_92_43 <- readxl::read_excel("../data/invertebrates_92_43.xlsx", skip=2, col_names=F)
+
+colnames(invertebrates_92_43) <- c("no","area","SPECIES_NAME","SPECIES_ID","ORDER","CLASS","PRIORITY","ANNEX_II","ANNEX_IV","ANNEX_V","KD","POPULATION_TREND","POPULATION_SIZE_UNIT","OCCURRENCE","SD")
+species_names <- unique(invertebrates_92_43$SPECIES_NAME) 
+
+#### this list contains species names from multiple versions of Monitoring, EPOPTEIA I, EPOPTEIA II. 
+species_names_combined <- c(
+  "Apatura metis",
+  "Astacus astacus",
+  "Austropotamobius torrentium",
+  "Bolbelasmus unicornis",
+  "Buprestis splendens",
+  "Callimorpha (Euplagia, Panaxia) quadripunctaria",
+  "Catopta thrips",
+  "Cerambyx cerdo",
+  "Coenagrion ornatum",
+  "Cordulegaster heros",
+  "Dioszeghyana schmidtii",
+  "Eriogaster catax",
+  "Euphydryas (Eurodryas, Hypodryas) aurinia",
+  "Euphydryas aurinia",
+  "Euplagia quadripunctaria",
+  "Hirudo medicinalis",
+  "Hirudo verbana",
+  "Hyles hippophaes",
+  "Lindenia tetraphylla",
+  "Lucanus cervus",
+  "Lycaena dispar",
+  "Maculinea arion",
+  "Morimus asper funereus",
+  "Morimus funereus",
+  "Ophiogomphus cecilia",
+  "Osmoderma eremita",
+  "Osmoderma eremita Complex",
+  "Papilio alexanor",
+  "Paracaloptenus caloptenoides",
+  "Parnassius apollo",
+  "Parnassius mnemosyne",
+  "Polyommatus eroides",
+  "Probaticus subrugosus",
+  "Proserpinus proserpina",
+  "Pseudophilotes bavius",
+  "Rhysodes sulcatus",
+  "Rosalia alpina",
+  "Stenobothrus (Stenobothrodes) eurasius",
+  "Stenobothrus eurasius",
+  "Stylurus flavipes",
+  "Unio crassus",
+  "Unio elongatulus",
+  "Vertigo angustior",
+  "Vertigo moulinsiana",
+  "Zerynthia polyxena"
+)
+##### Gbif data
+
+gbif_species_occ <- read_delim("../results/gbif_species_occ.tsv", delim="\t") |>
+    mutate(datasetName="Gbif")
+
+# Define the bounding box coordinates
+xmin <- 19.37359
+ymin <- 34.80202
+xmax <- 29.64306
+ymax <- 41.7485
+
+gbif_species_occ_gr <- gbif_species_occ |>
+    filter(decimalLongitude > 19.37359 & decimalLongitude<29.64306,
+           decimalLatitude>34.80202 & decimalLatitude < 41.7485 )
+
+######## NECCA compilation of previous Monitoring Data not included in ENVECO database
+E1X_MDPP_2014_2024_samples_data <- read_xlsx("../data/Ε1Χ_ΒΔ_ΠΡΩΤΟΓΕΝΩΝ_ΦΔ+ΜΔΠΠ_2014-2024.xlsx",
+                           sheet="Δείγματα Ασπόνδυλων",
+                           col_names=T) |> slice(-1) |> 
+    mutate(decimalLatitude=as.numeric(`Γεωγραφικό Πλάτος (WGS84) Αρχη`),
+           decimalLongitude=as.numeric(`Γεωγραφικό Μήκος (WGS84) Αρχή`)) |> 
+    filter(!is.na(decimalLongitude)) |>
+    mutate(datasetName="E1X_MDPP_2014_2024") |>
+    mutate(basisOfRecord="MATERIAL_SAMPLE")
+
+
+E1X_MDPP_2014_2024_species_data <- read_xlsx("../data/Ε1Χ_ΒΔ_ΠΡΩΤΟΓΕΝΩΝ_ΦΔ+ΜΔΠΠ_2014-2024.xlsx",
+                           sheet="Είδη",
+                           col_names=T
+                           ) |> slice(-1)
+
+E1X_MDPP_2014_2024_all <- E1X_MDPP_2014_2024_species_data |>
+    mutate(species=if_else(`Όνομα είδους`=="Άλλο",
+                           `Άλλο είδος`,
+                           `Όνομα είδους`)) |>
+    mutate(art17_92_43_EEC=if_else(`Όνομα είδους`!="Άλλο",
+                                   TRUE,
+                                   FALSE)) |>
+    mutate(individualCount=as.numeric(`Αριθμός ατόμων είδους`)) |>
+    left_join(E1X_MDPP_2014_2024_samples_data, by=c("Sam_ID"="Sam_ID"))
+
+######### previous monitoring from ENVECO
+##### references
+E1X_DB_ref_samples_data <- read_xlsx("../data/Ε1Χ_ΒΔ_ΒΙΒΛΙΟΓΡΑΦΙΑΣ_ΑΣΠ_20241204.xlsx",
+                                    sheet="Εξάπλωση ειδών και τ.ο.",
+                                    col_names=T) |> slice(-1) |> filter(!is.na(`Κωδικός Αναφοράς`))
+
+E1X_DB_refs_data <- read_xlsx("../data/Ε1Χ_ΒΔ_ΒΙΒΛΙΟΓΡΑΦΙΑΣ_ΑΣΠ_20241204.xlsx",
+                                    sheet="Βιβλιογραφία",
+                                    col_names=T) |> slice(-1) |> filter(!is.na(`Κωδικός Αναφοράς`))
+
+
+E1X_DB_ref_samples_data$decimalLatitude <- as.numeric(E1X_DB_ref_samples_data$`Γεωγραφικό Πλάτος (WGS84)`)
+E1X_DB_ref_samples_data$decimalLongitude <- as.numeric(E1X_DB_ref_samples_data$`Γεωγραφικό Μήκος (WGS84)`)
+E1X_DB_ref_samples_data$species <- E1X_DB_ref_samples_data$`Ονομασία είδους`
+
+E1X_DB_ref_all <- E1X_DB_ref_samples_data |>
+    left_join(E1X_DB_refs_data,
+              by=c("Κωδικός Αναφοράς"="Κωδικός Αναφοράς")) |>
+    mutate(datasetName="E1X_DB_references") |>
+    mutate(basisOfRecord="MaterialCitation") |>
+    mutate(species=`Ονομασία είδους`) |>
+    mutate(individualCount=as.numeric(`Πλήθος ατόμων`))
+
+
+##### samplings
+E1X_DB_samples_data <- read_xlsx("../data/Ε1Χ_ΒΔ_ΠΡΩΤΟΓΕΝΩΝ_ΥΠ4_ΑΣΠΟΝΔΥΛΑ_20241204.xlsx",
+                                    sheet="Δείγματα Ασπόνδυλων",
+                                    col_names=T) |> slice(-1)
+
+E1X_DB_samples_data$decimalLatitude <- as.numeric(deigmata_data_previous$`Γεωγραφικό Πλάτος (WGS84) Αρχη`)
+E1X_DB_samples_data$decimalLongitude <- as.numeric(deigmata_data_previous$`Γεωγραφικό Μήκος (WGS84) Αρχή`)
+
+E1X_DB_species_data <- read_xlsx("../data/Ε1Χ_ΒΔ_ΠΡΩΤΟΓΕΝΩΝ_ΥΠ4_ΑΣΠΟΝΔΥΛΑ_20241204.xlsx",
+                                    sheet="Είδη",
+                                    col_names=T) |> slice(-1) 
+
+E1X_DB_all <- E1X_DB_species_data |> 
+    filter(!is.na(Sam_ID)) |> 
+    left_join(E1X_DB_samples_data, by=c("Sam_ID"="Sam_ID")) |>
+    mutate(individualCount=as.numeric(`Αριθμός ατόμων είδους`)) |>
+    mutate(species=`Όνομα είδους`) |>
+    mutate(datasetName = "E1X_DB") |>
+    mutate(basisOfRecord="MATERIAL_SAMPLE")
+
+
+E1X_DB_select <- E1X_DB_all |>
+    filter(individualCount>0)
+
+######################## other private data
+
+Invertebrates_records_Olga <- read_delim("../data/Invertebrates_records_Olga_20250316.csv", delim=",") |>
+    mutate(decimalLongitude=Longitude,
+           decimalLatitude=Latitude,
+           species=Species,
+           individualCount=as.numeric(Individuals)) |>
+    mutate(datasetName = "Invertebrates_records_Olga") |>
+    mutate(basisOfRecord="MATERIAL_SAMPLE")
+
+########################### IUCN
+#
+#iucn <- read_delim("../data/redlist_species_data_dbd309b7-fb96-4f22-91c2-f184787ada27/points_data.csv", delim=",")
+#
+#iucn_92_43 <- iucn |>
+#    filter(sci_name %in% species_names)
+#
+#iucn_parnassius <- iucn |>
+#    filter(sci_name=="Parnassius apollo")
+
+
+############################# Species data integration ###################
+### common column names
+columns_to_keep <- c("species",
+                     "decimalLatitude",
+                     "decimalLongitude",
+                     "datasetName",
+                     "basisOfRecord",
+                     "individualCount")
+
+species_samples_integration <- list(gbif_species_occ_gr,
+                                    E1X_MDPP_2014_2024_all,
+                                    E1X_DB_select,
+                                    E1X_DB_ref_all,
+                                    Invertebrates_records_Olga) |>
+    map(~ dplyr::select(.x, all_of(columns_to_keep))) |>
+    bind_rows()
+
+write_delim(species_samples_integration, "../results/species_samples_integration.tsv",delim="\t")
+
+species_samples_art17 <- species_samples_integration |>
+    filter(species %in% species_names_combined)
+
+species_samples_art17_sf <- species_samples_art17 |>
+    filter(!is.na(decimalLongitude)) |>
+    st_as_sf(coords=c("decimalLatitude","decimalLongitude"),
+             remove=F,
+             crs="WGS84")
+
+species_with_data <- unique(species_samples_art17_sf$species)
+
+for (i in seq_along(species_with_data)){
+    species_occurrences <- species_samples_art17_sf |>
+        filter(species==species_with_data[i])
+    print(species_with_data[i])
+
+    
+    species_gr_map <- ggplot() +
+        geom_sf(greece_regions, mapping=aes()) +
+        geom_point(species_occurrences,
+                mapping=aes(x=decimalLongitude,
+                            y=decimalLatitude,
+                            color=datasetName),
+                size=1.8,
+                alpha=0.8,
+                show.legend=T) +
+        coord_sf(crs="WGS84") +
+        theme_bw()+
+        theme(axis.title=element_blank(),
+              axis.text=element_text(colour="black"),
+              legend.title = element_text(size=8),
+              legend.position = "bottom",
+              legend.box.background = element_blank())
+    
+    ggsave(paste0("../figures/map_", species_with_data[i], "_occurrences.png", sep=""), 
+           plot=species_gr_map, 
+           height = 20, 
+           width = 20,
+           dpi = 300, 
+           units="cm",
+           device="png")
+
+}
+
+
+
+
+
+
