efcee90d94f866972f4992e8a497bde212828bc2
savvas-paragkamian <s.paragkamian@gmail.com>
2025-04-23 07:52:57 +0300

    new taxonomic retrieval of species; gbif new download based on taxa; taxonomy curation

--- scripts/data_retrieval_preparation.R
+++ scripts/data_retrieval_preparation.R
@@ -21,13 +21,90 @@ library(rnaturalearth)
 ###
 greece_regions <- sf::st_read("../spatial_data/gadm41_GRC_shp/gadm41_GRC_2.shp")
 
-######################## GBIF ########################
-# GBIF retrieve data for all arthropod species that have been assessed in IUCN
-### NOT run takes time. 
+########################## species information ##############################
+# the species of the previous epopteia 2015
+
+invertebrates_92_43 <- readxl::read_excel("../data/invertebrates_92_43.xlsx", skip=2, col_names=F)
+
+colnames(invertebrates_92_43) <- c("no","area","SPECIES_NAME","SPECIES_ID","ORDER","CLASS","PRIORITY","ANNEX_II","ANNEX_IV","ANNEX_V","KD","POPULATION_TREND","POPULATION_SIZE_UNIT","OCCURRENCE","SD")
+
+species_names <- unique(invertebrates_92_43$SPECIES_NAME) 
+
+#### this list contains species names from multiple versions of Monitoring, EPOPTEIA I, EPOPTEIA II. 
+
+species_names_combined <- c(
+  "Apatura metis",
+  "Astacus astacus",
+  "Austropotamobius torrentium",
+  "Bolbelasmus unicornis",
+  "Buprestis splendens",
+  "Callimorpha (Euplagia, Panaxia) quadripunctaria",
+  "Callimorpha quadripunctaria",
+  "Catopta thrips",
+  "Paracossulus thrips",
+  "Cerambyx cerdo",
+  "Coenagrion ornatum",
+  "Cordulegaster heros",
+  "Dioszeghyana schmidtii",
+  "Eriogaster catax",
+  "Euphydryas (Eurodryas, Hypodryas) aurinia",
+  "Euphydryas aurinia",
+  "Euplagia quadripunctaria",
+  "Hirudo medicinalis",
+  "Hirudo verbana",
+  "Hyles hippophaes",
+  "Lindenia tetraphylla",
+  "Lucanus cervus",
+  "Lycaena dispar",
+  "Maculinea arion",
+  "Morimus asper funereus",
+  "Morimus funereus",
+  "Ophiogomphus cecilia",
+  "Osmoderma eremita",
+  "Osmoderma eremita Complex",
+  "Papilio alexanor",
+  "Paracaloptenus caloptenoides",
+  "Parnassius apollo",
+  "Parnassius mnemosyne",
+  "Polyommatus eroides",
+  "Probaticus subrugosus",
+  "Proserpinus proserpina",
+  "Pseudophilotes bavius",
+  "Rhysodes sulcatus",
+  "Rosalia alpina",
+  "Stenobothrus (Stenobothrodes) eurasius",
+  "Stenobothrus eurasius",
+  "Stylurus flavipes",
+  "Unio crassus",
+  "Unio elongatulus",
+  "Vertigo angustior",
+  "Vertigo moulinsiana",
+  "Zerynthia polyxena"
+)
+
+######################## Species taxonomy ########################
+### gnr verifier to resolve species names
+gnr_species_verifier <- gna_verifier(species_names_combined,
+                                     all_matches=T,
+                                     data_sources=c(1,9,11,12)) #Catalogue of Life, Worms, Gbif,EOL
+
+write_delim(gnr_species_verifier, "../results/gnr_species_verifier.tsv",delim="\t")
+
+species_gbif_taxonomy_search <- name_backbone_checklist(species_names_combined,verbose=TRUE)
+write_delim(species_gbif_taxonomy_search, "../results/species_gbif_taxonomy_search.tsv",delim="\t")
+
+species_gbif_taxonomy <- species_gbif_taxonomy_search |>
+    filter(is_alternative=="FALSE")
+
+write_delim(species_gbif_taxonomy, "../results/species_gbif_taxonomy_f.tsv",delim="\t")
+
 #species_names <- as.character(invertebrates_all_species$SPECIES_NAME)
 #gbif_species <- get_gbifid(species_names,ask=F)
 
 
+######################## GBIF ########################
+# GBIF retrieve data for all arthropod species that have been assessed in IUCN
+### NOT run takes time. 
 #species_gbif_df <- data.frame(sci_name=species_names, gbifid=gbif_species)
 #write_delim(species_gbif_df, "../results/species_gbif_invertebrates.tsv", delim="\t")
 #### takes even more time!!!
@@ -44,34 +121,35 @@ greece_regions <- sf::st_read("../spatial_data/gadm41_GRC_shp/gadm41_GRC_2.shp")
 #
 #write_delim(classification_species_d, "../results/classification_species_gbif.tsv", delim="\t")
 #
-# Resolve names
-## gnr_datasources() |> filter(title=="GBIF Backbone Taxonomy") id=11
-#gnr_species <- gnr_resolve(species_gbif_df$sci_name)
-#gnr_species_gbif <- gnr_resolve(species_gbif_df$sci_name, data_source_ids=11)
-#
-#write_delim(gnr_species, "../results/gnr_species_names.tsv", delim="\t")
 #
 ########################## species occurrences ##############################
 # GBIF occurrences
 ## need to set GBIF credential in the .Renviron file
+gbif_taxon_df <- gnr_species_verifier |>
+    filter(dataSourceId==11) 
+
+gbif_taxon_keys <- as.numeric(gbif_taxon_df$recordId)
+gbif_taxon_ids_interactive <- get_gbifid(species_names_combined)
 
 #gbif_taxon_keys <- as.numeric(na.omit(species_gbif_df$gbifid.ids))
 #
 ## run once to request the download from the server
-#occ_download(
-#pred_in("taxonKey", gbif_taxon_keys), # important to use pred_in
-#pred("hasCoordinate", TRUE),
-#pred("hasGeospatialIssue", FALSE),
-#format = "SIMPLE_CSV"
-#)
+occ_download(
+pred_in("taxonKey", gbif_taxon_keys), # important to use pred_in
+pred("country", "GR"),
+pred("hasCoordinate", TRUE),
+pred("hasGeospatialIssue", FALSE),
+format = "SIMPLE_CSV"
+)
 
 # to check the status of the download
 # This is for the species of annex II 
-# occ_download_wait('0026745-241024112534372') 
-# the key for the gbif download of 268 invertegrate species 
+occ_download_wait('0010753-250415084134356')
+# GBIF.org (23 April 2025) GBIF Occurrence Download  https://doi.org/10.15468/dl.v7ruda
+# Another key for the gbif download of 268 invertegrate species 
 # is 0018673-241107131044228 
 
-#gbif_species_occ <- occ_download_get('0018673-241107131044228') |>
+#gbif_species_occ <- occ_download_get('0010753-250415084134356') |>
 #    occ_download_import()
 
 #write_delim(gbif_species_occ, "../results/gbif_invertebrate_species_occ.tsv", delim="\t")
@@ -86,8 +164,8 @@ gbif_species_occ_sf <- gbif_species_occ |>
 
 greece_regions_bbox <- st_as_sf(st_as_sfc(st_bbox(greece_regions)))
 
-gbif_species_world_map <- ggplot() +
-    geom_sf(world, mapping=aes()) +
+gbif_species_map <- ggplot() +
+    geom_sf(greece_regions, mapping=aes()) +
     geom_point(gbif_species_occ_sf,
             mapping=aes(x=decimalLongitude,
                         y=decimalLatitude,
@@ -95,7 +173,6 @@ gbif_species_world_map <- ggplot() +
             size=1.8,
             alpha=0.8,
             show.legend=T) +
-    geom_sf(greece_regions, mapping=aes()) +
     coord_sf(crs="WGS84") +
     theme_bw()+
     theme(axis.title=element_blank(),
@@ -104,13 +181,13 @@ gbif_species_world_map <- ggplot() +
           legend.position = "bottom",
           legend.box.background = element_blank())
 
-#ggsave("../figures/gbif_species_world_map.png", 
-#       plot=gbif_species_world_map, 
-#       height = 40, 
-#       width = 60,
-#       dpi = 300, 
-#       units="cm",
-#       device="png")
+ggsave("../figures/gbif_species_map.png", 
+       plot=gbif_species_map, 
+       height = 40, 
+       width = 60,
+       dpi = 300, 
+       units="cm",
+       device="png")
 
 ## Greece only
 
@@ -125,6 +202,7 @@ gbif_species_occ_gr <- gbif_species_occ_sf |>
            decimalLatitude>34.80202 & decimalLatitude < 41.7485 )
 
 gbif_species_gr_map <- ggplot() +
+    geom_sf(greece_regions, mapping=aes()) +
     geom_point(gbif_species_occ_gr,
             mapping=aes(x=decimalLongitude,
                         y=decimalLatitude,
@@ -132,7 +210,6 @@ gbif_species_gr_map <- ggplot() +
             size=1.8,
             alpha=0.8,
             show.legend=T) +
-    geom_sf(greece_regions, mapping=aes()) +
     coord_sf(crs="WGS84") +
     theme_bw()+
     theme(axis.title=element_blank(),
